157c15074eb4553b7bbd76e6c4a728e0e49ea2fb
michaeljh17 <mih279@student.cpit.ac.nz>
2015-02-21 08:30:04 +1300

    Revert "Trying to solve a bug which occurs when the program"
    This reverts commit 84ceddfe764e5fb65ae56bb896443af906d006ae.

--- RocketReadingApp/src/localStorageController.js
+++ RocketReadingApp/src/localStorageController.js
@@ -8,29 +8,14 @@ var localStorageController =  {
     },
     
     saveAllGamesData : function () {
-        "use strict";
         var player = rocketReadingModel.getMyPlayer();
-        player.setAllGamesData(rocketReadingModel.getAllGamesData());
-        console.groupCollapsed("localStorageController : saveAllGamesData()");
-        console.log("player userName: " + player.userName);
-        console.log("player firstName: " + player.firstName);
-        console.log("player lastName: " + player.lastName);
-        console.log("player school: " + player.school);
-        console.log("player classRoom: " + player.classRoom);
-        console.log("player totalScore: " + player.totalScore);
-        console.log("player levelGameReached: " + player.levelGameReached);
-        console.log("player bonusGameReached: " + player.bonusGameReached);
-        console.log("player pointsToPassLevel: " + player.pointsToPassLevel);
-        console.log("player currentGameData: " + player.currentGameData);
-        console.log("player allGamesData: " + player.allGamesData);
-        console.groupEnd();
+        player.allGamesData = rocketReadingModel.getAllGamesData();
         localStorage.setItem(rocketReadingModel.getMyPlayer().getUserName(), JSON.stringify(player));
     },
     
     saveCurrentGameData : function () {
-        "use strict";
         var player = rocketReadingModel.getMyPlayer();
-        player.setCurrentGameData(rocketReadingModel.getCurrentGameData());
+        player.currentGameData = rocketReadingModel.getCurrentGameData();
         localStorage.setItem(rocketReadingModel.getMyPlayer().getUserName(), JSON.stringify(player));
     },
     
